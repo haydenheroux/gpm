3417d4a5da95a30026143d5d4329463fa1f45a0f
Hayden Heroux <haydenheroux@gmail.com>
2023-06-20 22:53:24 -0400

    Improve styling a bit more.

--- main.go
+++ main.go
@@ -58,39 +58,60 @@ func (procs Processes) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 var (
-	border = lipgloss.NewStyle().BorderStyle(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("240"))
+	borderStyle = lipgloss.NewStyle().BorderStyle(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("240"))
+	aliveStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("10"))
+	deadStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("9"))
+
+	cellColor = lipgloss.Color("255")
+	cellStyle = lipgloss.NewStyle().Foreground(cellColor)
 )
 
 func (procs Processes) View() string {
 	columns := []table.Column{
-		{Title: "id", Width: 6},
+		{Title: "id", Width: 16},
 		{Title: "name", Width: 22},
 		{Title: "version", Width: 16},
-		{Title: "pid", Width: 20},
+		// TODO This width is super sensitive because of the styling
+		{Title: "alive", Width: 18},
+		{Title: "pid", Width: 16},
 		{Title: "uptime", Width: 20},
-		{Title: "alive", Width: 8},
 	}
 
 	rows := []table.Row{}
 
 	for _, proc := range procs.procs {
-		id := fmt.Sprint(proc.Id)
+		id := cellStyle.Render(fmt.Sprint(proc.Id))
 		name := proc.Name
 		version := proc.Version
-		pid := fmt.Sprint(proc.pid)
-		uptime := time.Since(proc.startTime).String()
-		alive := fmt.Sprint(proc.Alive)
-		rows = append(rows, table.Row{id, name, version, pid, uptime, alive})
+
+		var alive, pid, uptime string
+
+		if proc.Alive {
+			alive = aliveStyle.Render("alive")
+			pid = fmt.Sprint(proc.pid)
+			uptime = time.Since(proc.startTime).String()
+		} else {
+			alive = deadStyle.Render("dead")
+			pid = ""
+			uptime = ""
+		}
+
+		rows = append(rows, table.Row{id, name, version, alive, pid, uptime})
 	}
 
-	table := table.New(
+	t := table.New(
 		table.WithColumns(columns),
 		table.WithRows(rows),
-		table.WithFocused(true),
-		table.WithHeight(10),
+		table.WithFocused(false),
+		table.WithHeight(len(procs.procs)),
 	)
 
-	return border.Render(table.View()) + "\n"
+	s := table.DefaultStyles()
+	s.Header = s.Header.BorderStyle(lipgloss.NormalBorder()).BorderForeground(lipgloss.Color("240")).BorderBottom(true).Bold(true)
+	s.Selected  = s.Selected.Foreground(cellColor).Bold(false)
+	t.SetStyles(s)
+
+	return borderStyle.Render(t.View()) + "\n"
 }
 
 type tickMsg time.Time
